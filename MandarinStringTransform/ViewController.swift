eb587eeddc7b81e6d3a572bfb16a176006a74f85
Song Xiaoming <song.ryan90@gmail.com>
2015-03-29 02:28:58 +0800

    get spelling of mandaren

--- MandarinStringTransform/ViewController.swift
+++ MandarinStringTransform/ViewController.swift
@@ -8,18 +8,43 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate, UITextViewDelegate {
 
+    @IBOutlet weak var field: UITextField!
+    @IBOutlet weak var result: UITextView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        field.delegate = self
+        result.delegate = self
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+// MARK:- UITextFieldDelegate
+    
+    func textFieldDidEndEditing(textField: UITextField) {
+     
+        var text = textField.text as CFString;
+        var string = CFStringCreateMutableCopy(kCFAllocatorDefault, 0, text)
+        
+        CFStringTransform(string, nil, kCFStringTransformMandarinLatin, 0);
 
-
+        CFStringTransform(string, nil, kCFStringTransformStripDiacritics, 0);
+        
+        result.text = string;
+        
+    }
+    
+    
+    override func touchesBegan(touches: NSSet, withEvent event: UIEvent) {
+        self.view.endEditing(true)
+    }
+    
 }
 
